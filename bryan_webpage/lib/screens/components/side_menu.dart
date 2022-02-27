839b4e12e5404792f2333af159a6a0c9e8e8eb0d
Bryan <bryanvaz14@gmail.com>
2022-02-27 16:43:54 -0700

    Knowledge widget added

--- bryan_webpage/lib/screens/components/side_menu.dart
+++ bryan_webpage/lib/screens/components/side_menu.dart
@@ -1,6 +1,7 @@
 import 'package:bryan_webpage/constants.dart';
 import 'package:bryan_webpage/screens/components/skills.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'area_info_text.dart';
 import 'coding.dart';
 import 'my_info.dart';
@@ -37,10 +38,53 @@ class SideMenu extends StatelessWidget {
                 height: defaultPadding,
               ),
               Coding(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Divider(),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: defaultPadding),
+                    child: Text(
+                      "Knowledge",
+                      style: Theme.of(context).textTheme.subtitle2,
+                    ),
+                  ),
+                  KnowledgeText(text: "Agile methodologies"),
+                  KnowledgeText(text: "GIT"),
+                  KnowledgeText(text: "Basic UX/UI Design"),
+                  KnowledgeText(text: "SQL"),
+                ],
+              ),
             ],
           ),
         ))
       ]),
+    );
+  }
+}
+
+class KnowledgeText extends StatelessWidget {
+  const KnowledgeText({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: defaultPadding),
+      child: Row(
+        children: [
+          SvgPicture.asset("icons/Check.svg"),
+          SizedBox(
+            width: defaultPadding / 2,
+          ),
+          Text(text)
+        ],
+      ),
     );
   }
 }
